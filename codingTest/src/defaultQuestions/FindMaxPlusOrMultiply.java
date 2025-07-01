0d7e696bf8319f98af8c5a4791e57e61c31fe7ee
임PRO스터\su992 <su9926@naver.com>
2025-07-01 17:37:10 +0900

    곱하기 or 더하기 문제
    -  0 혹은 양의 정수로만 이루어진 배열이 있을 때, 왼쪽부터 오른쪽으로 하나씩 모든 숫자를 확인하며 숫자 사이에 '✕' 혹은 '+' 연산자를 넣어 결과적으로 가장 큰 수를 구하는 프로그램
    - 모든 연산은 왼쪽에서 순서대로 이루어짐

---
+++ codingTest/src/defaultQuestions/FindMaxPlusOrMultiply.java
@@ -0,0 +1,17 @@
+package defaultQuestions;
+
+public class FindMaxPlusOrMultiply {
+    public static void main(String[] args) {
+        System.out.println(findMaxResult(new int[]{0,3,5,6,1,2,4}));
+        System.out.println(findMaxResult(new int[]{3,2,1,5,9,7,4}));
+        System.out.println(findMaxResult(new int[]{1,1,1,3,3,2,5}));
+    }
+
+    public static int findMaxResult(int[] array){
+        int result = array[0];
+        for(int i=1; i<array.length; i++){
+            result = Math.max(array[i]*result, array[i]+result);
+        }
+        return result;
+    }
+}
