32cea86d75f759045ab42ffd3997319a25228871
임PRO스터\su992 <su9926@naver.com>
2025-07-09 16:09:42 +0900

    문자열 뒤집기
    - 핵심: 문자열을 0과 1이 연속된 그룹으로 나눈 뒤, 그룹의 개수가 더 적은 쪽을 뒤집는 게 최소 횟수이다. 이때 모든 숫자가 동일할 경우 뒤집을 필요가 없기 때문에 답은 0이다.
    - https://www.acmicpc.net/problem/1439

---
+++ codingTest/src/defaultQuestions/StringReversal.java
@@ -0,0 +1,38 @@
+package codingTest.src.defaultQuestions;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class StringReversal {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        System.out.println(cntStringReversal(input));
+    }
+
+    public static int cntStringReversal(String input) {
+        if(input.length()==1) return 0;
+        int cntOne = 0, cntZero = 0;
+        char prevChar = input.charAt(0);
+        if(prevChar=='0'){
+            cntZero++;
+        }else{
+            cntOne++;
+        }
+        for (int i = 1, n = input.length(); i < n; i++) {
+            char currentChar = input.charAt(i);
+            if(prevChar!=currentChar){
+                if(currentChar=='0'){
+                    cntZero++;
+                }else{
+                    cntOne++;
+                }
+                prevChar=currentChar;
+            }
+        }
+
+        return Math.min(cntOne, cntZero);
+    }
+}
