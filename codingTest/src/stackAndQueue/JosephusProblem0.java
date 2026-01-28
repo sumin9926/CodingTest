f6253fedf751f29bd29ede62065bafd7cbb1f600
임PRO스터\su992 <su9926@naver.com>
2026-01-28 13:51:52 +0900

    'Queue' 연습용 문제

---
+++ codingTest/src/stackAndQueue/JosephusProblem0.java
@@ -0,0 +1,33 @@
+package codingTest.src.stackAndQueue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class JosephusProblem0 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()), K = Integer.parseInt(st.nextToken());
+        Deque<Integer> dq = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder("<");
+
+        for (int i = 1; i <= N; i++) {
+            dq.offerLast(i);
+        }
+
+        while (!dq.isEmpty()) {
+            for (int i = 0; i < K - 1; i++) {
+                dq.offerLast(dq.removeFirst());
+            }
+            sb.append(dq.removeFirst());
+            if(!dq.isEmpty()) sb.append(", ");
+            else sb.append(">");
+        }
+
+        System.out.println(sb);
+    }
+}
